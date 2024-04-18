20dbbd3b0d44072362163eb36626bcf4432bf82d
natanika76 <natali.nickitina2011@yandex.ru>
2024-04-18 15:05:20 +0300

    Канцтовары

---
+++ DataAccess.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DZ3
+{
+    public class DataAccess
+    {
+        private readonly string connectionString;
+
+        public DataAccess(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        //Название канцтоваров, которые не продавались заданное количество дней:
+        public List<string> GetProductsNotSoldForDays(int days)
+        {
+            List<string> products = new List<string>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("GetProductsNotSoldForDays", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Days", days);
+
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string productName = reader.GetString(0);
+                            products.Add(productName);
+                        }
+                    }
+                }
+            }
+
+            return products;
+        }
+
+        //Информация о менеджере по продажам с наибольшей общей суммой прибыли за указанный промежуток времени:
+        public int GetTopManagerBySalesProfitInRange(DateTime startDate, DateTime endDate)
+        {
+            int topManagerID = -1;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("GetTopManagerBySalesProfitInRange", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@StartDate", startDate);
+                    command.Parameters.AddWithValue("@EndDate", endDate);
+
+                    connection.Open();
+
+                    object result = command.ExecuteScalar();
+                    if (result != null && result != DBNull.Value)
+                    {
+                        topManagerID = Convert.ToInt32(result);
+                    }
+                }
+            }
+
+            return topManagerID;
+        }
+
+        // Вставка новых канцтоваров
+        public void InsertNewProduct(string productName, string productType, decimal unitPrice)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("INSERT INTO Products (ProductName, ProductType, UnitPrice) VALUES (@ProductName, @ProductType, @UnitPrice)", connection))
+                {
+                    command.Parameters.AddWithValue("@ProductName", productName);
+                    command.Parameters.AddWithValue("@ProductType", productType);
+                    command.Parameters.AddWithValue("@UnitPrice", unitPrice);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Вставка новых типов канцтоваров
+        public void InsertNewProductType(string productType)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("INSERT INTO ProductTypes (ProductType) VALUES (@ProductType)", connection))
+                {
+                    command.Parameters.AddWithValue("@ProductType", productType);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Вставка новых менеджеров по продажам
+        public void InsertNewManager(string managerName)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("INSERT INTO Managers (ManagerName) VALUES (@ManagerName)", connection))
+                {
+                    command.Parameters.AddWithValue("@ManagerName", managerName);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Вставка новых фирм покупателей
+        public void InsertNewCustomer(string customerName)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("INSERT INTO Customers (CustomerName) VALUES (@CustomerName)", connection))
+                {
+                    command.Parameters.AddWithValue("@CustomerName", customerName);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Обновление информации о существующих канцтоварах
+        public void UpdateProduct(int productID, string productName, string productType, decimal unitPrice)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("UPDATE Products SET ProductName = @ProductName, ProductType = @ProductType, UnitPrice = @UnitPrice WHERE ProductID = @ProductID", connection))
+                {
+                    command.Parameters.AddWithValue("@ProductID", productID);
+                    command.Parameters.AddWithValue("@ProductName", productName);
+                    command.Parameters.AddWithValue("@ProductType", productType);
+                    command.Parameters.AddWithValue("@UnitPrice", unitPrice);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Обновление информации о существующих фирмах покупателях
+        public void UpdateCustomer(int customerID, string customerName)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("UPDATE Customers SET CustomerName = @CustomerName WHERE CustomerID = @CustomerID", connection))
+                {
+                    command.Parameters.AddWithValue("@CustomerID", customerID);
+                    command.Parameters.AddWithValue("@CustomerName", customerName);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Обновление информации о существующих менеджерах по продажам
+        public void UpdateManager(int managerID, string managerName)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("UPDATE Managers SET ManagerName = @ManagerName WHERE ManagerID = @ManagerID", connection))
+                {
+                    command.Parameters.AddWithValue("@ManagerID", managerID);
+                    command.Parameters.AddWithValue("@ManagerName", managerName);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Обновление информации о существующих типах канцтоваров
+        public void UpdateProductType(int productTypeID, string productType)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("UPDATE ProductTypes SET ProductType = @ProductType WHERE ProductTypeID = @ProductTypeID", connection))
+                {
+                    command.Parameters.AddWithValue("@ProductTypeID", productTypeID);
+                    command.Parameters.AddWithValue("@ProductType", productType);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Удаление канцтоваров
+        public void DeleteProduct(int productID)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("DELETE FROM Products WHERE ProductID = @ProductID", connection))
+                {
+                    command.Parameters.AddWithValue("@ProductID", productID);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Удаление менеджеров по продажам
+        public void DeleteManager(int managerID)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("DELETE FROM Managers WHERE ManagerID = @ManagerID", connection))
+                {
+                    command.Parameters.AddWithValue("@ManagerID", managerID);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Удаление типов канцтоваров
+        public void DeleteProductType(int productTypeID)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("DELETE FROM ProductTypes WHERE ProductTypeID = @ProductTypeID", connection))
+                {
+                    command.Parameters.AddWithValue("@ProductTypeID", productTypeID);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Удаление фирм покупателей
+        public void DeleteCustomer(int customerID)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("DELETE FROM Customers WHERE CustomerID = @CustomerID", connection))
+                {
+                    command.Parameters.AddWithValue("@CustomerID", customerID);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        public void SetIdentityInsert(string tableName, bool value)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"SET IDENTITY_INSERT {tableName} {(value ? "ON" : "OFF")}";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=Stationery; Integrated Security=SSPI;";
+
+            // Создание экземпляра DataAccess для выполнения операций с базой данных
+            DataAccess dataAccess = new DataAccess(connectionString);
+            /*
+                        // Вызываем методы для вставки данных
+                        InsertSampleData(dataAccess);
+
+                        // Вызываем методы для обновления данных
+                        UpdateData(dataAccess);
+
+                        // Вызываем методы для удаления данных
+                        DeleteData(dataAccess);
+            */
+            // Вызываем методы для получения данных
+            GetProductsNotSoldForDaysExample(dataAccess);
+            GetTopManagerBySalesProfitInRangeExample(dataAccess);
+
+        }
+
+        static void InsertSampleData(DataAccess dataAccess)
+        {
+        
+            // Вставка новых канцтоваров
+            dataAccess.InsertNewProduct("Pen", "Writing", 1.5m);
+            dataAccess.InsertNewProduct("Notebook", "Stationery", 3.0m);
+
+            // Вставка новых типов канцтоваров
+            dataAccess.InsertNewProductType("Art Supplies");
+            dataAccess.InsertNewProductType("Office Supplies");
+
+            // Вставка новых менеджеров по продажам
+            dataAccess.InsertNewManager("John Doe");
+            dataAccess.InsertNewManager("Jane Smith");
+
+            // Вставка новых фирм покупателей
+            dataAccess.InsertNewCustomer("ABC Company");
+            dataAccess.InsertNewCustomer("XYZ Corporation");
+           
+        }
+
+        static void UpdateData(DataAccess dataAccess)
+        {
+            // Обновление информации о существующем канцтоваре
+            dataAccess.UpdateProduct(1, "Ballpoint Pen", "Writing", 2.0m);
+
+            // Обновление информации о существующем клиенте
+            dataAccess.UpdateCustomer(1, "New ABC Company Name");
+
+            // Обновление информации о существующем менеджере
+            dataAccess.UpdateManager(1, "John Smith");
+
+            // Обновление информации о существующем типе канцтовара
+            dataAccess.UpdateProductType(1, "School Supplies");
+        }
+
+        static void DeleteData(DataAccess dataAccess)
+        {
+            // Удаление канцтовара
+            dataAccess.DeleteProduct(2);
+
+            // Удаление клиента
+            dataAccess.DeleteCustomer(2);
+
+            // Удаление менеджера
+            dataAccess.DeleteManager(2);
+
+            // Удаление типа канцтовара
+            dataAccess.DeleteProductType(2);
+        }
+
+        static void GetProductsNotSoldForDaysExample(DataAccess dataAccess)
+        {
+            int days = 30; // Указываем количество дней
+
+            Console.WriteLine($"Products not sold for {days} days:");
+            var products = dataAccess.GetProductsNotSoldForDays(days);
+            foreach (var product in products)
+            {
+                Console.WriteLine(product);
+            }
+        }
+
+        static void GetTopManagerBySalesProfitInRangeExample(DataAccess dataAccess)
+        {
+            DateTime startDate = new DateTime(2024, 1, 1); // Указываем начальную дату
+            DateTime endDate = new DateTime(2024, 3, 31); // Указываем конечную дату
+
+            int topManagerID = dataAccess.GetTopManagerBySalesProfitInRange(startDate, endDate);
+
+            Console.WriteLine($"Top manager ID by sales profit in range ({startDate.ToShortDateString()} - {endDate.ToShortDateString()}): {topManagerID}");
+        }
+    }
+}
+/*
+ CREATE TABLE Managers (
+    ManagerID INT PRIMARY KEY,
+    ManagerName NVARCHAR(50)
+);
+
+CREATE TABLE Sales (
+    SaleID INT PRIMARY KEY,
+    ManagerID INT,
+    SaleDate DATE,
+    SaleAmount DECIMAL(10,2),
+    Quantity INT,
+    ProductID INT,
+    FOREIGN KEY (ManagerID) REFERENCES Managers(ManagerID)
+);
+
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY IDENTITY,
+    ProductName NVARCHAR(50),
+    ProductType NVARCHAR(50),
+    UnitPrice DECIMAL(10,2)
+);
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    CustomerName NVARCHAR(50)
+);
+
+CREATE TABLE Purchases (
+    PurchaseID INT PRIMARY KEY,
+    CustomerID INT,
+    SaleID INT,
+    PurchaseDate DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
+    FOREIGN KEY (SaleID) REFERENCES Sales(SaleID)
+);
+
+CREATE PROCEDURE GetProductsNotSoldForDays
+    @Days INT
+AS
+BEGIN
+    SELECT ProductName
+    FROM Products
+    WHERE ProductID NOT IN (
+        SELECT DISTINCT ProductID
+        FROM Sales
+        WHERE DATEDIFF(DAY, SaleDate, GETDATE()) <= @Days
+    );
+END;
+
+CREATE PROCEDURE GetTopManagerBySalesProfitInRange
+    @StartDate DATE,
+    @EndDate DATE
+AS
+BEGIN
+    SELECT TOP 1 ManagerID
+    FROM Sales
+    WHERE SaleDate BETWEEN @StartDate AND @EndDate
+    GROUP BY ManagerID
+    ORDER BY SUM(SaleAmount) DESC;
+END;
+ */
